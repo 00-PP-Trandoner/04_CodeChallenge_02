8b235d3f900bd98ac93a005162b423eedaaf55ae
Normann <trand_oner@gmx.de>
2023-07-17 09:02:01 +0200

    Challenge Update

--- challenge_02.js
+++ challenge_02.js
@@ -16,4 +16,23 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+const firstName = prompt("bitte Namen eingeben: ");
+const age = parseInt(prompt("Bitte alter eingeben: "));
 
+let drink;
+
+if (!isNaN(age) && age >= 0) {
+    if (age <= 5) {
+        drink = "Milch"
+;
+    } else if (age <= 12) {
+        drink = "Saft";
+    } else if (age <= 17) {
+        drink = "Cola";
+    } else {
+        drink = "Wein";
+    }
+    console.log(`${firstName} trinkt ${drink}`);
+}   else {
+    console.log ("Tee");
+}
